a87b60bb9a81eca15c03c4c2860da52aa8684259
Axelntw <ntwaliaxel@gmail.com>
2025-06-01 14:59:00 +0000

    Task 4

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,25 +1,54 @@
-import PostCard from '../../components/common/PostCard';
-import Header from '../../components/layout/Header';
-import Footer from '../../components/layout/Footer';
-//import { PostProps }", "export async function getStaticProps()", "https://jsonplaceholder.typicode.com/posts
+import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
+import Header from "@/components/layout/Header";
+import { PostData, PostProps } from "@/interfaces";
+import { useState } from "react";
 
-const Posts: React.FC = () => {
+const Posts: React.FC<PostProps[]> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [post, setPost] = useState<PostData | null>(null);
+
+
+  const handleAddPost = (newPost: PostData) => {
+    setPost({ ...newPost, id: posts.length + 1 });
+  };
+
+
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="flex flex-col h-screen">
       <Header />
-      <main className="flex-grow container mx-auto px-4 py-8">
-        <h1 className="text-3xl font-bold mb-6">Posts</h1>
-        <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-          <PostCard 
-            title="First Post"
-            content="This is the content of the first post."
-            author="John Doe"
-          />
+      <main className="p-4">
+        <div className="flex justify-between">
+          <h1 className=" text-2xl font-semibold">Post Content</h1>
+          <button onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {
+            posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+              <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+            ))
+          }
         </div>
       </main>
-      <Footer />
+
+      {isModalOpen && (
+        <PostModal onClose={() => setModalOpen(false)} onSubmit={handleAddPost} />
+      )}
     </div>
-  );
-};
+  )
+}
+
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
 
 export default Posts;
